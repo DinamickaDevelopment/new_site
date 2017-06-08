12737e9ec14574b29a3e5820a4067203c5dac324
DinamickaDevelopment <info@dinamicka.com>
2017-06-08 20:11:59 +0300

    first commit

---
+++ js/home.js
@@ -0,0 +1,35 @@
+$(document).ready(() => {
+
+    const resizeFunc = () => {
+        $('.homepage__content_tab').css({
+            height: window.innerHeight + 'px', 
+            width: window.innerWidth - 250 + 'px'
+        })
+
+        $('.small-tab').css({
+            height: window.innerHeight / 2.6 + 'px'
+        })
+
+        $('.big-tab').css({
+            height: ((window.innerHeight / 2.6)*2) + 'px'
+        })
+    } 
+
+    resizeFunc(); 
+    window.addEventListener('resize', (e) => {
+        resizeFunc(); 
+    }) 
+
+    $(document).ready(function() {
+	    $('#pagepiling').pagepiling({
+            anchors: ['#1', '#2', '#3', '#4'], 
+            navigation: false
+        });
+    });
+
+    $('.scroll-down-btn').on('click', () => {
+        let host = window.location.protocol + window.location.hostname + '#2'; 
+        window.location.replace(host); 
+    })
+  
+})
